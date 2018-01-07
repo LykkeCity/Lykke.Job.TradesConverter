1c7960fc39084c393cadee087e3d45e857807b95
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-01-07 10:44:54 +0300

    Added messagepack atributes to fee contract class

--- contract/Lykke.Job.TradesConverter.Contract/TradeLogItemFee.cs
+++ contract/Lykke.Job.TradesConverter.Contract/TradeLogItemFee.cs
@@ -1,15 +1,23 @@
-﻿namespace Lykke.Job.TradesConverter.Contract
+﻿using MessagePack;
+
+namespace Lykke.Job.TradesConverter.Contract
 {
+    [MessagePackObject]
     public class TradeLogItemFee
     {
+        [Key(0)]
         public string Type { get; set; }
 
+        [Key(1)]
         public string SourceClientId { get; set; }
 
+        [Key(2)]
         public string TargetClientId { get; set; }
 
+        [Key(3)]
         public string SizeType { get; set; }
 
+        [Key(4)]
         public double? Size { get; set; }
     }
 }
